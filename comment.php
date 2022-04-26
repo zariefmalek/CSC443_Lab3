1790063ce5403f5ff8fd2345bce92cc44258ca45
zariefmalek <2020462506@student.uitm.edu.my>
2022-04-27 00:37:46 +0800

    comment.php

---
+++ comment.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <?php
+   //This is a comment
+   //echo "X equals $x" ;
+   $x += 10; // Increment $x by 10
+   echo $x;
+   /* This is a section 
+   of multiline comments
+   that will not be 
+   interpreted */
+   ?>
+</body>
+</html>    
