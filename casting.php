51643e8392db626c8113809bd6ec9de6641d8684
zariefmalek <2020462506@student.uitm.edu.my>
2022-04-27 00:52:55 +0800

    casting.php

---
+++ casting.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Basic PHP Syntax</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $int = 12;
+    $double = 10.37;
+    $int = $int + $double;
+    echo $int;
+    ?>
+
+</body>
+</html>
