fe6c31d81a42396572d4df65842e0139fbedacc5
zariefmalek <2020462506@student.uitm.edu.my>
2022-04-27 01:08:54 +0800

    string.php

---
+++ string.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $txt1 = "Hello there, World!";
+    $txt2 = "Have a nice day!";
+    $greet = $txt1 . " " . $txt2
+    echo $greet;?>
+    echo "<br />";
+    echo strlen ("Hello world!");
+    echo "<br />";
+    echo strpos ("Hello world!", "world");
+    echo "<br />";
+    ?>
+    
+
+</body>
+</html>
