e3325c0a03b7bcb1dd076de97a577a033ab86225
zariefmalek <2020462506@student.uitm.edu.my>
2022-04-27 00:47:41 +0800

    variable.php

---
+++ variable.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $username = "Fred Smith"
+    echo $username;
+    echo "<br />";
+    $current_user = $username;
+    echo $current_user;
+    echo "<br />";
+    $num1 = 10.5;
+    echo $num1;
+    ?>
+</body>
+</html>
